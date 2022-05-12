eff55cf545c1d6582fc304b6b10c06c3cd9e9146
QuitteriePilon <quitteriep3@gmail.com>
2022-05-12 16:29:50 +0200

    version 1 FAST finished

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,6 +10,14 @@ using namespace std;
 const int WIDTH = 640;
 const int HEIGHT = 480;
 
+// Convert RGB image to grayscale using the luminosity method
+void RGBtoGrayScale(Mat rgb, Mat *grayscale)
+{
+    std::vector<Mat> channels(3);
+    split(rgb, channels);
+    *grayscale = (0.07 * channels[0] + 0.72 * channels[1] + 0.21 * channels[2]);
+}
+
 Mat createImgStraightLines()
 {
     Mat image(HEIGHT, WIDTH, CV_8UC3, Scalar(255, 255, 255));
@@ -19,120 +27,136 @@ Mat createImgStraightLines()
     return image;
 }
 
-//N : nombre de pixels regardés pour savoir si le pixel p est un point clefs
+// N : nombre de pixels regardés pour savoir si le pixel p est un point clefs vector<Point> * fewer_kps
 
-// void FAST(Point ** fewer_kps,Mat img,int N=9, float threshold=0.15, int nms_window=2)
-// {
-//     //Application du filtre gaussien 3*3
-//     Mat img_filtered;
-//     GaussianBlur(img,img_filtered,Size(3,3),0);
-
-//     Mat cross_idx=(Mat_<int>(2,4) << 3,0,-3,0,0,3,0,-3);
-//     Mat circle_idx=(Mat_<int>(2,16)<<3,3,2,1,0,-1,-2,-3,-3,-3,-2,-1,0,1,2,3,0,1,2,3,3,3,2,1,0,-1,-2,-3,-3,-3,-2,-1);
-
-//     Mat corner_img=Mat::zeros(img.size(),CV_64FC1); //Attention si img pas mat 2d ne fonctionne pas donc à revoir
-//     vector<Point> keypoints;
-
-//     for(int x=3;i<img.size[0]-3;i++)
-//     {
-//         for(int y=3;j<img.size[1]-3;j++)
-//         {
-//             auto intensity=img[x][y];
-//             if(threshold<1)
-//             {
-//                 threshold=threshold*pixel;
-//             }
-//             int darker_cpt=0;
-//             int brighter_cpt=0;
-//             //Vérification des pixels 1,5,9,13
-//             for(int pos_circle=0;pos_circle<cross_idx.size[1];pos_circle++)
-//             {
-//                 if(img[x+cross_idx[0,pos_circle],y+cross_idx[1,pos_circle]] > (intensity+threshold)) 
-//                 {
-//                    darker_cpt++;
-//                 }
-//                 if(img[x+cross_idx[0,pos_circle],y+cross_idx[1,pos_circle]] < (intensity-threshold))
-//                 {
-//                     brighter_cpt++;
-//                 }
-//             }
-//             if(darker_cpt>=3 || brighter_cpt>=3)
-//             {
-//                 darker_cpt=0;
-//                 brighter_cpt=0;
-//                 //Si c'est condition est respectée, alors on peut vérifier pour les autres points
-//                 for(int pos_circle=0;pos_circle<circle_idx.size[1];pos_circle++)
-//                 {
-//                     if(img[x+circle_idx[0,pos_circle],y+circle_idx[1,pos_circle]] > (intensity+threshold)) 
-//                     {
-//                         darker_cpt++;
-//                     } 
-//                     else
-//                     {
-//                         darker_cpt=0;
-//                     }
-//                     if(img[x+circle_idx[0,pos_circle],y+circle_idx[1,pos_circle]] < (intensity-threshold)) 
-//                     {
-//                         brighter_cpt++;
-//                     } 
-//                     else
-//                     {
-//                         brighter_cpt=0;
-//                     }
-//                     if(brighter_cpt>=N || darker_cpt>=N)
-//                     {
-//                         break;
-//                     }
-//                 }
-//                 //On ajoute le point clef si il y a plus de N pixels d'affilés qui sont soient plus foncés soient plus claires que p
-//                 if(darker_cpt>=N || brighter_cpt>=N)
-//                 {
-//                     Point pt;
-//                     pt.x=y;
-//                     pt.y=x;
-//                     keypoints.push_back(pt); //Keypoint [y,x]=[col,row]
-//                     corner_img[x,y]=abs(intensity-img[x+circle_idx[0],y+circle_idx[1]]).sum();
-//                 }
-//             }
-
-            
-//         }
-//     }
-// }
-
-int main(int argc, char const *argv[])
+vector<Point> myFast(Mat &img, int N = 9, float threshold = 0.15, int nms_window = 2)
 {
+    // Application du filtre gaussien 3*3
+    Mat img_filtered;
+    GaussianBlur(img, img_filtered, Size(3, 3), 0);
+
+    Mat cross_idx = (Mat_<int>(2, 4) << 3, 0, -3, 0, 0, 3, 0, -3);
+    // On double les coordonnées des pixels du cercle afin de pouvoir faire un peu plus d'un tour dans le cas ou les pixels qui se suivent
+    // commencent à la fin du cercle et se poursuivent au début
+    Mat circle_idx = (Mat_<int>(2, 32) << 3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -2, -1, 0, 1, 2, 3, 3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -2, -1, 0, 1, 2, 3,
+                      0, 1, 2, 3, 3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -2, -1, 0, 1, 2, 3, 3, 3, 2, 1, 0, -1, -2, -3, -3, -3, -2, -1);
+
+    Mat corner_img = Mat::zeros(img.size(), CV_8UC1); // Attention si img pas mat 2d ne fonctionne pas donc à revoir
+    vector<Point> keypoints;
+
+    for (int x = 3; x < img.rows - 3; x++)
+    {
+        const uchar *ptr_img = img.ptr<uchar>(x);
+
+        for (int y = 3; y < img.cols - 3; y++)
+        {
+            auto intensity_pixel = ptr_img[y];
+            /*if (threshold < 1)
+            {
+                threshold = threshold * intensity_pixel;
+            }
+            */
+            int darker_cpt = 0;
+            int brighter_cpt = 0;
+            // Vérification des pixels 1,5,9,13
+            // ATTENTION plus fonce -> valeur pixel plus grande donc en vrai plus clair
+            for (int pos_circle = 0; pos_circle < cross_idx.cols; pos_circle++)
+            {
+                const uchar *ptr_img_cross = img.ptr<uchar>(x + cross_idx.at<int>(0, pos_circle));
+                auto pixel = ptr_img_cross[y + cross_idx.at<int>(1, pos_circle)];
+                if (ptr_img_cross[y + cross_idx.at<int>(1, pos_circle)] > (intensity_pixel + threshold))
+                {
+                    darker_cpt++;
+                }
+                if (ptr_img_cross[y + cross_idx.at<int>(1, pos_circle)] < (intensity_pixel - threshold))
+                {
+                    brighter_cpt++;
+                }
+            }
+            if (darker_cpt >= 3 || brighter_cpt >= 3)
+            {
+                darker_cpt = 0;
+                brighter_cpt = 0;
+                // Si c'est condition est respectée, alors on peut vérifier pour les autres points
+                int nbIterCircle = circle_idx.cols / 2 + N;
+                for (int pos_circle = 0; pos_circle < nbIterCircle; pos_circle++)
+                {
+                    const uchar *ptr_img_circle = img.ptr<uchar>(x + circle_idx.at<int>(0, pos_circle));
+
+                    if (ptr_img_circle[y + circle_idx.at<int>(1, pos_circle)] > (intensity_pixel + threshold))
+                    {
+                        darker_cpt++;
+                    }
+                    else
+                    {
+                        darker_cpt = 0;
+                    }
+                    if (ptr_img_circle[y + circle_idx.at<int>(1, pos_circle)] < (intensity_pixel - threshold))
+                    {
+                        brighter_cpt++;
+                    }
+                    else
+                    {
+                        brighter_cpt = 0;
+                    }
+                    if (brighter_cpt >= N || darker_cpt >= N)
+                    {
+                        break;
+                    }
+                }
+                // On ajoute le point clef si il y a plus de N pixels d'affilés qui sont soient plus foncés soient plus claires que p
+                if (darker_cpt >= N || brighter_cpt >= N)
+                {
+                    Point pt;
+                    pt.x = y;
+                    pt.y = x;
+                    keypoints.push_back(pt); // Keypoint [y,x]=[col,row]
+                    int nbIterCircle = circle_idx.cols / 2;
+                    for (int j = 0; j < nbIterCircle; j++)
+                    {
+                        corner_img.at<int>(x, y) += abs(intensity_pixel - img.at<uchar>(x + circle_idx.at<int>(0, j), y + circle_idx.at<int>(1, j)));
+                    }
+                }
+            }
+        }
+    }
+    return keypoints;
+}
+
+int main(int argc, char *argv[])
+{
+    loguru::init(argc, argv);
     LOG_F(INFO, "Start orb test application");
 
     LOG_F(INFO, "Create image");
     auto img = createImgStraightLines();
 
     LOG_F(WARNING, "Display image on screen");
+    // imshow("The best window ever", img);
+    // waitKey(0);
+
+    LOG_F(INFO, "End of the orb test application");
+    std::cout << "Bonjour tout le monde !.\n";
+
+    Vec3b color{255, 0, 0};
+    img.at<Vec3b>(Point(3, 3)) = color;
+
+    Mat grayscale = Mat::zeros(Size(img.rows, img.cols), CV_8UC1);
+    cvtColor(img, grayscale, COLOR_BGR2GRAY);
+    imshow("The best window ever", grayscale);
+    waitKey(0);
+
+    vector<Point> KeyPoint = myFast(grayscale, 12, 10, 2);
+
+    LOG_F(INFO, "fini myfast \n");
+    cout << KeyPoint;
+
+    for (Point &point : KeyPoint)
+    {
+        img.at<Vec3b>(point) = color;
+    }
     imshow("The best window ever", img);
     waitKey(0);
 
-    LOG_F(INFO, "End of the orb test application");
-    std::cout<<"Bonjour tout le monde !.\n"; 
-
-    Mat cross_idx=(Mat_<int>(2,4) << 3,0,-3,0,0,3,0,-3);
-    Mat circle_idx=(Mat_<int>(2,16)<<3,3,2,1,0,-1,-2,-3,-3,-3,-2,-1,0,1,2,3,0,1,2,3,3,3,2,1,0,-1,-2,-3,-3,-3,-2,-1);
-
-    std::cout<<cross_idx.size()<<"\n";
-    std::cout<<cross_idx.size[0]<<"\n";
-    std::cout<<cross_idx.rows<<"\n";
-
-    for(int i=0;i<cross_idx.rows;i++)
-    {
-        
-        const int *pi=cross_idx.ptr<int>(i);
-        for(int j=0;j<cross_idx.cols;j++)
-        {
-            cout<<pi[j]<<"\n";
-        }
-    }
-    for(int j=0;j<cross_idx.cols;j++)
-        {
-        cout<<cross_idx.at<int>(0,j)<<","<<cross_idx.at<int>(1,j)<<"\n";
-        }
     return 0;
 }
